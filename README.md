e5c3bb515c148fed5d6f6255d7bbc5dd1899f52d
sludnovaaa <sludnovaaa@altarix.ru>
2020-04-05 17:11:04 +0400

    README для Лёхи

---
+++ README.md
@@ -0,0 +1,11 @@
+# ЧТО ЖЕ ЭТО ТУТ У НАС
+ЛУЧШИЙ НА СВЕТЕ ПРОЕКТТТТТТ С ЖАБАМИ
+
+Окружение в toads_env.yml, перед запуском:
+- conda env update -f toads_env.yml
+- conda activate ToadEnv
+
+ПРОФИТ
+
+*detect_single_threaded.py* - основная часть программы
+*test.py* - отражатель жаб
--- test.py
+++ test.py
@@ -12,12 +12,12 @@ for file in path[0][2]:
     size = max(image.shape)
     n_image = np.full((size, size, 3), 255).astype(np.float32)
     n_image[0:image.shape[0], 0:image.shape[1]] = image
-    n_image = cv2.resize(n_image, (200, 200))
+    n_image = cv2.resize(n_image, (1024, 1024))
     cv2.imwrite(pth1+'/'+file.replace('.jpg', '-n.jpg'), n_image)
-    n_image_15 = imutils.rotate_bound(n_image, 15)
-    cv2.imwrite(pth1 + '/' + file.replace('.jpg', '-n15.jpg'), n_image_15)
+    #n_image_15 = imutils.rotate_bound(n_image, 15)
+    #cv2.imwrite(pth1 + '/' + file.replace('.jpg', '-n15.jpg'), n_image_15)
     #n_image__15 = imutils.rotate_bound(n_image, -15)
     #cv2.imwrite(pth1 + '/' + file.replace('.jpg', '-n_15.jpg'), n_image__15)
 
-    #nn_image = cv2.flip(n_image, 1)
-    #cv2.imwrite(pth1 + '/' + file.replace('.jpg', '-nn.jpg'), nn_image)
+    nn_image = cv2.flip(n_image, 1)
+    cv2.imwrite(pth1 + '/' + file.replace('.jpg', '-nn.jpg'), nn_image)
